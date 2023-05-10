1ef3dfc500650e88a27ec4955addc0c391ab5a27
Anand Bose <anandbose16@gmail.com>
2023-05-11 00:01:41 +0530

    Remove unused fonts, add Mastodon verification element

--- src/main/kotlin/components/Footer.kt
+++ src/main/kotlin/components/Footer.kt
@@ -4,6 +4,22 @@ import com.katalyst.dom.BodyContext
 
 fun BodyContext.includeSiteFooter() {
     footer {
-        span("&copy;2023 Anand Bose. All rights reserved.")
+        text("&copy;2023 Anand Bose. All rights reserved.")
+        br()
+        a(href = "https://github.com/droidista", text = "GitHub")
+        text(" ")
+        a(href = "https://gitlab.com/anandbose", text = "Gitlab")
+        text(" ")
+        a(
+            href = "https://mastodon.social/@anandbose",
+            text = "Mastodon",
+            customAttributes = mapOf("rel" to "me"),
+        )
+        text(" ")
+        a(
+            href = "https://pixelfed.social/@anandbose",
+            text = "Pixelfed",
+            customAttributes = mapOf("rel" to "me"),
+        )
     }
 }
