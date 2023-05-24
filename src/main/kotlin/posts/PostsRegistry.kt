16ed68cb0425c9d13cb162cd8a4b97758078f916
Anand Bose <anandbose16@gmail.com>
2023-05-24 07:59:24 +0530

    Remove post y2023/may

--- src/main/kotlin/posts/PostsRegistry.kt
+++ src/main/kotlin/posts/PostsRegistry.kt
@@ -2,10 +2,8 @@ package posts
 
 import posts.y2018.august.buildGrpcVsJson06Aug2018
 import posts.y2020.may.buildAndroidKeystoreRotation08May2020
-import posts.y2023.may.buildGoodOldDaysOfSocialMedia09May2023
 
 fun getAllPosts() = listOf(
     buildAndroidKeystoreRotation08May2020(),
     buildGrpcVsJson06Aug2018(),
-    buildGoodOldDaysOfSocialMedia09May2023(),
 )
